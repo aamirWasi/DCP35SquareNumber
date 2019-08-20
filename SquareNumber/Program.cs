ba2f9bd113a204db45a60352804b8eeb4d21c041
aamir <wasi3aamir@gmail.com>
2019-08-21 01:32:16 +0600

    got runtimeerror 17times

--- SquareNumber/Program.cs
+++ SquareNumber/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SquareNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 1; t <= T; t++)
+            {
+                var numberToCheck = int.Parse(Console.ReadLine());
+                double result = Math.Sqrt(numberToCheck);
+                bool isSquare = result % 1 == 0;
+                if (isSquare)
+                {
+                    Console.WriteLine($"Case {t}: YES");
+                }
+                else
+                {
+                    Console.WriteLine($"Case {t}: NO");
+                }
+            }
         }
     }
 }
