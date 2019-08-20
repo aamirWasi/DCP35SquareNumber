21e9d6988fb4de9935faab279cb2a61fb2487bae
aamir <wasi3aamir@gmail.com>
2019-08-21 01:50:17 +0600

    Accepted DCP147Brother's Challange

---
+++ SquareNumber/DCP147BrotherChallange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SquareNumber
+{
+    public class DCP147BrotherChallange
+    {
+        static void Main()
+        {
+            uint input = uint.Parse(Console.ReadLine());
+            uint sum = 0;
+            for (uint i = 0; i < input; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
