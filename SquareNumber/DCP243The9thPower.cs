f8d992ae646f901fcfab17a746149997f0fd47b0
aamir <wasi3aamir@gmail.com>
2019-08-21 02:27:20 +0600

    Failed DCP243The9thPower

---
+++ SquareNumber/DCP243The9thPower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SquareNumber
+{
+    public class DCP243The9thPower
+    {
+        static void Main()
+        {
+            int T = int.Parse(Console.ReadLine());
+            double result;
+            for (int t = 0; t < T; t++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                double ninthPower = Math.Pow(9, input);
+                result = ninthPower % 10;
+                Console.WriteLine($"{result}");
+            }
+        }
+    }
+}
