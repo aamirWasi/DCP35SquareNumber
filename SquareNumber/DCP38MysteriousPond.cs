0628abbc22bc688560af3b9fa74894b2b1780cf1
aamir <wasi3aamir@gmail.com>
2019-08-21 04:05:11 +0600

    DCP38MysteriousPond  accepted

---
+++ SquareNumber/DCP38MysteriousPond.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace SquareNumber
+{
+    public class DCP38MysteriousPond
+    {
+        static void Main()
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                int RobiFriends = int.Parse(Console.ReadLine());
+                string minName = string.Empty;
+                string maxName = string.Empty;
+                int high = int.MinValue;
+                int low = int.MaxValue;
+                for (int i = 0; i < RobiFriends; i++)
+                {
+                    string input = Console.ReadLine();
+                    var inputArray = input.Split(' ');
+                    string name = inputArray[0];
+                    var getValue = int.Parse(inputArray[1]);
+                    if (getValue > high)
+                    {
+                        high = getValue;
+                        maxName = name;
+                    }
+                    if (getValue < low)
+                    {
+                        low = getValue;
+                        minName = name;
+                    }
+                }
+                Console.WriteLine($"{maxName} {minName}");
+            }
+        }
+    }
+}
